0ddce0e80b95a92e27a747817c398a8c64e0274d
pickaxe_engineer <pickaxe.engineer@gmail.com>
2020-06-26 11:04:42 +0200

    Added documentation and prepared custom modelled block

--- src/main/java/com/example/examplemod/blocks/SimpleBlock.java
+++ src/main/java/com/example/examplemod/blocks/SimpleBlock.java
@@ -8,6 +8,22 @@ import net.minecraft.block.material.MaterialColor;
 
 /**
  * A very basic solid cube with nothing special.
+ * <p>
+ *     This block's name is set during registration, see {@link AthenaBlockRegistry},
+ *     however for convenience, it's a constant {@link #REGISTRY_NAME}.
+ * </p>
+ * <h2>Externally required files</h2>
+ * <ul>
+ *     <li>The blockstate JSON: {@code resources/assets/}{@link com.example.examplemod.AthenaMod#MODID}{@code /blockstates/}{@link #REGISTRY_NAME}{@code .json}</li>
+ *     <li>The model JSON: {@code resources/assets/}{@link com.example.examplemod.AthenaMod#MODID}{@code /models/block/}{@link #REGISTRY_NAME}{@code .json}</li>
+ *     <li>The texture JSON: {@code resources/assets/}{@link com.example.examplemod.AthenaMod#MODID}{@code /textures/block/}{@link #REGISTRY_NAME}{@code .json} (in this particular instance, the model specifies a minecraft vanilla texture</li>
+ *     <li>A localisation entry in {@code resources/assets/}{@link com.example.examplemod.AthenaMod#MODID}{@code /lang/en_us.json} with the key {@code block.}{@link com.example.examplemod.AthenaMod#MODID}{@code .}{@link #REGISTRY_NAME}</li>
+ * </ul>
+ * <h2>Each Block requires a {@link net.minecraft.item.BlockItem}</h2>
+ * See {@link AthenaBlockRegistry}
+ * <ul>
+ *     <li>The item model JSON: {@code resources/assets/}{@link com.example.examplemod.AthenaMod#MODID}{@code /models/item/}{@link #REGISTRY_NAME}{@code .json}</li>
+ * </ul>
  *
  * @author pickaxe_engineer
  */
